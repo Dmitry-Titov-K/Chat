e457dc2110e0362f841de5d1a9bd939be29ab4c3
Dima <titovdmitryk@gmail.com>
2020-12-22 14:08:53 +0300

    create message

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-
+import { Message } from 'components'
 import './Home.scss'
 
 const Home = () => {
 
     return (
         <section className='home'>
-            <div>
-                Hello
-            </div>
+            <Message 
+                avatar='https://www.flaticon.com/svg/static/icons/svg/3930/3930551.svg' 
+                text='Как разработать свой чат на ReactJS, Redux + NodeJS, Socket.IO, MongoDB.' 
+                date='Tue Dec 22 2020 13:38:57 GMT+0300' />
         </section>
     )
 }
