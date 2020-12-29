10f2f400a9d8adeb99dc3157af5e6891db4ec0f8
Dima <titovdmitryk@gmail.com>
2020-12-29 16:32:02 +0300

    login refact

--- src/modules/LoginForm/components/LoginForm.jsx
+++ src/modules/LoginForm/components/LoginForm.jsx
@@ -1,66 +1,106 @@
 import React from 'react'
 import { Form, Input } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { UserOutlined, LockOutlined, InfoCircleTwoTone } from '@ant-design/icons';
 import {NavLink} from 'react-router-dom'
 import { Block, Button } from 'components'
+import PropTypes from 'prop-types';
+
+const LoginForm = props=> {
 
 
-const LoginForm = () => {
-
-    return (
-        <div>
-            <div className="auth__top">
+    
+        const success = true
+        const {
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+        }=props
+        
+        return (
+            <div>
+                <div className="auth__top">
                     <h2>Войти в аккаунт</h2>
                     <p>пожалуйста, войдите в свой аккаунт
                    </p>
                 </div>
                 <Block>
-
-                    <Form
+    
+                    {success ? <Form onSubmit={handleSubmit}
                         name="normal_login"
                         className="login-form"
-                        initialValues={{
-                            remember: true,
-                        }}
-
+                       
+    
                     >
+                        
                         <Form.Item
                             name="username"
-                            hasFeedback
                             
-                           
+                            hasFeedback  
                         >
-                            <Input size='large' prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                            <Input size='large' 
+                            prefix={<UserOutlined className="site-form-item-icon" />} 
+                            placeholder="Ваше имя" />
                         </Form.Item>
                         <Form.Item
                             name="password"
+                            validateStatus={!touched.password  ? '' : errors.password ? "error":"success"}
                             hasFeedback
-                           
+                            allowClear
+                            help={!touched.password  ? null : errors.password }
                         >
-                            <Input
+                            <Input.Password
+                                id='password'
                                 size='large'
                                 prefix={<LockOutlined className="site-form-item-icon" />}
                                 type="password"
-                                placeholder="Password"
+                                placeholder="Пароль"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.email}
+                                
                             />
                         </Form.Item>
-
-
+                        
+    
+    
                         <Form.Item>
-                            <Button type="primary" size='large'  htmlType="submit" >
-                                Войти в аккаунт
-                            </Button>
-
+                            <Button onClick={handleSubmit} type="primary" size='large' htmlType="submit" >
+                                Зарегистрироваться
+                                </Button>
+    
                         </Form.Item>
                         <Form.Item>
-                            <NavLink to='/register' className="auth__register-link">Зарегистрироваться</NavLink>
+                            <NavLink to='/login' className="auth__register-link">Войти в аккаунт</NavLink>
                         </Form.Item>
-                    </Form>
-
+                    </Form> : <div className='auth__success-block'>
+    
+                            <div><InfoCircleTwoTone/> </div>
+                            <h2>Подтвердите свой аккаунт</h2>
+                            <p>На Вашу почту отправлено писмо с ссылкой на подтверждение аккаунта</p>
+                        </div>
+                    }
                 </Block>
-        </div>
-            
-    )
-}
+            </div>
+    
+        )
+    }
+    
+
+
+    LoginForm.propTypes={
+        values:PropTypes.any,
+        errors:PropTypes.any,
+        touched: PropTypes.any,
+        handleBlur: PropTypes.any,
+        handleChange: PropTypes.any,
+        handleSubmit: PropTypes.any
+    }
+    
+
+   
+
 
 export default LoginForm
