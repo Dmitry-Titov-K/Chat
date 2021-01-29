80e78c5ddc301ac321b08593d4ecfd22772f9672
Dima <titovdmitryk@gmail.com>
2021-01-29 16:50:53 +0300

    !

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,17 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+//const middleware = [thunk];
+
+//console.log(middleware);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
