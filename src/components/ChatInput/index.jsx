3cd3306e52f798070141e9d9f56651f7992f5f3d
Dima <titovdmitryk@gmail.com>
2021-02-04 14:56:01 +0300

    add emojipicker

--- src/components/ChatInput/index.jsx
+++ src/components/ChatInput/index.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 import { Input, Button, Upload } from 'antd'
 import { SmileOutlined, AudioOutlined, SendOutlined, PaperClipOutlined } from "@ant-design/icons"
 
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
 
 import './ChatInput.scss'
 
-const SmileButton = () => {
+
+
+const ChatInput = () => {
+
+    const [pickerVision, setPickerVision] = useState(false)
+
+    const onPickerVisionToogle = () => {
+
+        setPickerVision(!pickerVision)
+
+    }
 
     return (
-        <React.Fragment>
-            <Upload>
+        <div className='chat-input'>
+            <div className="chat-input__emojiPicker"
+                onMouseLeave={() => setPickerVision()}>
+                {pickerVision && <Picker title={null} set='apple' />}
+            </div>
+
+
+            <Input size='large' prefix={
+                <Button
+                    onClick={() => onPickerVisionToogle()}
+                    onMouseEnter={() => onPickerVisionToogle()}
+                    type='link'><SmileOutlined /></Button>} />
+            <Upload showUploadList={false}>
                 <Button type='link'><PaperClipOutlined style={{ fontSize: '18px', color: '#000' }} />
                 </Button>
             </Upload>
@@ -18,17 +41,6 @@ const SmileButton = () => {
 
 
             <Button type='link'><AudioOutlined style={{ fontSize: '18px', color: '#000' }} /></Button>
-        </React.Fragment>
-    )
-}
-
-
-const ChatInput = () => {
-    return (
-        <div className='chat-input'>
-
-
-            <Input size='large' prefix={<Button type='link'><SmileOutlined /></Button>} suffix={<SmileButton />} />
             <Button type='link'>
                 <SendOutlined style={{ fontSize: '18px', color: '#000' }} />
             </Button>
