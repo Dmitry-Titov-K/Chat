ab90ef00a25280e7428bd0b5e729c32cc518d620
Dima <titovdmitryk@gmail.com>
2021-02-09 12:40:08 +0300

    add async/await

--- src/redux/actions/dialogs.js
+++ src/redux/actions/dialogs.js
@@ -9,11 +9,17 @@ const actions = {
     type: "DIALOGS:SET_CURRENT_DIALOG",
     payload: id,
   }),
-  fetchDialogs: () => (dispatch) => {
-    dialogsApi.getAllDialogs().then(({ data }) => {
-      dispatch(actions.setDialogs(data));
-    });
+  fetchDialogs: () => async (dispatch) => {
+    try {
+      const data = await dialogsApi.getAllDialogs();
+      return dispatch(actions.setCurrentDialog(data));
+    } catch (error) {
+      return error.message;
+    }
   },
 };
 
 export default actions;
+
+/* dialogsApi.getAllDialogs().then(({ data }) => {
+  dispatch(actions.setDialogs(data)); */
